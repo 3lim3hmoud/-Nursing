aeb2f230d8d57c5eae08052f8acd3331173cb5cc
3lim3hmoud <114960329+3lim3hmoud@users.noreply.github.com>
2026-06-27 17:04:01 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    غيّر رقم VERSION كل ما ترفع تحديث جديد
    مثلاً: v1 → v2 → v3 ...
 */
-const VERSION = 'v44';
+const VERSION = 'v45';
 const CACHE = 'nursing-hub-' + VERSION;
 
 const ASSETS = [
